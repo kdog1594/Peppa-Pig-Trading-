3566745314fb6c6a3a3b2f2998d043884dc4141a
Saabiq Ali <saabiqali@Saabiqs-MacBook-Air.local>
2021-07-11 18:14:57 +1000

    Functionvolume-Sharpe 6.11

--- PepperPigTrading.py
+++ PepperPigTrading.py
@@ -27,7 +27,8 @@ def getBetaCoeff(stock_cheaper, stock_expensive):
 
 
 def Strategy(prices):
-    myPositions = np.zeros(100, dtype = "i")      
+
+    myPositions = np.zeros(100, dtype = "i")
     
     pairs = [(11, 37), (19, 35), (30, 51), (30, 76), (44, 59), (44, 97), (45, 13), (45, 68), (45, 97), (46, 61), (46, 72), (46, 96), (48, 20), (48, 38), (50, 53), (50, 60), (50, 62), (50, 70), (50, 72), (50, 76), (50, 78), (50, 90), (51, 52), (51, 60), (51, 62), (51, 70), (51, 76), (51, 90), (52, 51), (52, 60), (52, 65), (52, 90), (52, 93), (53, 50), (53, 62), (53, 65), (53, 70), (53, 74), (53, 79), (53, 81), (53, 90), (53, 93), (53, 98), (53, 99), (54, 27), (54, 56), (54, 58), (54, 88), (54, 89), (54, 98), (55, 54), (55, 56), (55, 57), (55, 58), (55, 68), (55, 69), (55, 74), (55, 79), (55, 81), (55, 84), (55, 88), (55, 91), (55, 92), (55, 94), (55, 95), (55, 96), (55, 98), (56, 54), (56, 55), (56, 57), (56, 61), (56, 69), (56, 73), (56, 83), (56, 88), (56, 89), (56, 92), (56, 94), (56, 98), (57, 55), (57, 58), (57, 69), (57, 72), (57, 79), (57, 83), (57, 84), (57, 85), (57, 88), (57, 91), (57, 92), (57, 94), (57, 98), (57, 99), (58, 54), (58, 55), (58, 57), (58, 69), (58, 83), (58, 88), (58, 94), (58, 95), (58, 98), (58, 99), (60, 50), (60, 51), (60, 52), (60, 62), (60, 64), (60, 68), (60, 74), (60, 76), (60, 77), (60, 79), (60, 81), (60, 90), (61, 46), (61, 56), (61, 58), (61, 73), (61, 88), (61, 89), (61, 91), (61, 92), (61, 95), (62, 50), (62, 51), (62, 53), (62, 60), (62, 64), (62, 65), (62, 70), (62, 76), (62, 79), (62, 86), (62, 93), (63, 65), (63, 70), (63, 72), (63, 76), (63, 79), (63, 93), (64, 44), (64, 52), (64, 60), (64, 62), (64, 68), (64, 70), (64, 76), (64, 79), (64, 87), (64, 90), (64, 96), (65, 52), (65, 53), (65, 62), (65, 63), (65, 70), (65, 74), (65, 76), (65, 77), (65, 78), (67, 72), (67, 79), (67, 82), (67, 99), (68, 44), (68, 45), (68, 52), (68, 55), (68, 60), (68, 64), (68, 70), (68, 72), (68, 76), (68, 77), (68, 90), (69, 2), (69, 55), (69, 57), (69, 58), (69, 84), (69, 88), 
     (69, 92), (69, 94), (69, 96), (69, 98), (69, 99), (70, 50), (70, 51), (70, 53), (70, 62), (70, 64), (70, 65), (70, 68), (70, 72), (70, 74), (70, 76), (70, 81), (70, 90), (70, 96), (70, 99), (71, 53), (71, 70), (71, 79), (72, 46), (72, 50), (72, 63), (72, 67), (72, 68), (72, 70), (72, 76), (72, 79), (72, 83), (72, 91), (72, 93), (72, 96), (72, 98), (72, 99), (73, 55), (73, 56), (73, 61), (73, 92), (74, 50), (74, 52), (74, 55), (74, 60), (74, 65), (74, 70), (74, 76), (74, 78), (74, 79), (74, 86), (74, 90), (74, 96), (76, 50), (76, 51), (76, 59), (76, 60), 
@@ -67,63 +68,67 @@ def Strategy(prices):
 
         z_score  = (last_price_spread - mean_spread)/one_std
 
-        # Volume of Shares (Lots of $1250)
-        
-        sharesize_expensive = 1250 // curPrices[stock_expensive]
-        sharesize_cheaper = abs(b) * sharesize_expensive
+        # Volume of Shares
+       
+        #Dollar_volume = -3.369272 + 2311.321*abs(z_score) - 2156.334*(abs(z_score)**2)+ 1132.075*(abs(z_score)**3)
+
+        #Dollar_volume = -4 + 2300*abs(z_score) - 2100*(abs(z_score)**2)+ 1100*(abs(z_score)**3)
+
+        #Dollar_volume = 1500 + 7500 * (np.log(abs(z_score)))
+
+        #Dollar_volume = 1000 + 1000*(np.exp(abs(z_score))) (BEST ONE SO FAR - 6.11 Sharp, 2.042% return)
+
+        #Dollar_volume = 750 + 1000*(np.exp(abs(z_score))) (6.06 Sharp, 2.054% return)
+
+        #Dollar_volume = 500 + 1000*(np.exp(abs(z_score))) (6.00 sharp, 2.065% return)
+
+        #Dollar_volume = 1000 + 1100*(np.exp(abs(z_score))) (6.12 sharp, 2.029% return)
+
+        #Dollar_volume = 1000 + 900*(np.exp(abs(z_score))) (6.05 Sharp, 2.042% return)
+
+        Dollar_volume = 1000 + 1000*(np.exp(abs(z_score)))
+
+
+        sharesize_expensive = Dollar_volume // curPrices[stock_expensive]
+        sharesize_cheaper = (abs(b) * sharesize_expensive) // 1
 
         max_sharesize_cheaper = 10000 // curPrices[stock_cheaper]
         
         if abs(sharesize_cheaper) > abs(max_sharesize_cheaper):
             sharesize_cheaper = max_sharesize_cheaper
-        
+
+        if z_score >= 4.5: #STOPLOSS
+            myPositions[stock_cheaper] = 0 
+            myPositions[stock_expensive] = 0
+
+        elif z_score <= -4.5: #STOPLOSS
+            myPositions[stock_cheaper] = 0 
+            myPositions[stock_expensive] = 0
+
         # Shorting the Spread
 
-        if z_score >= 2.5: 
-            myPositions[stock_cheaper] = sharesize_cheaper * 8 
-            myPositions[stock_expensive] = -sharesize_expensive * 8
-        
-        elif z_score >= 2 and z_score < 2.5: 
-            myPositions[stock_cheaper] = sharesize_cheaper * 4
-            myPositions[stock_expensive] = -sharesize_expensive * 4
-
-        elif z_score >= 1 and z_score < 2:    
-            myPositions[stock_cheaper] = sharesize_cheaper * 2         
-            myPositions[stock_expensive] = -sharesize_expensive * 2
-
-        elif z_score >= 1 and z_score < 1.5:      
+        elif z_score >= 1 and z_score < 4.5:      
             myPositions[stock_cheaper] = sharesize_cheaper            
             myPositions[stock_expensive] = -sharesize_expensive
 
         # Longing the Spread
 
-        elif z_score <= -2.5: 
-            myPositions[stock_cheaper] = -sharesize_cheaper * 8 
-            myPositions[stock_expensive] = sharesize_expensive * 8
-        
-        elif z_score <= -2 and z_score > -2.5: 
-            myPositions[stock_cheaper] = -sharesize_cheaper * 4
-            myPositions[stock_expensive] = sharesize_expensive * 4
-
-        elif z_score <= -1.5 and z_score > -2:    
-            myPositions[stock_cheaper] = -sharesize_cheaper * 2         
-            myPositions[stock_expensive] = sharesize_expensive * 2
-
-        elif z_score <= -1 and z_score > -1.5:      
+        elif z_score <= -1 and z_score > -4.5:      
             myPositions[stock_cheaper] = -sharesize_cheaper            
             myPositions[stock_expensive] = sharesize_expensive
 
         # Take Profit
             
-        elif z_score <= (1/2.5) and z_score >=0: 
+        elif z_score <= (1/3) and z_score >=0: 
             myPositions[stock_cheaper] = 0
             myPositions[stock_expensive] = 0
 
-        elif z_score >= (-1/2.5) and z_score <=0:
+        elif z_score >= (-1/3) and z_score <=0:
             myPositions[stock_cheaper] = 0 
             myPositions[stock_expensive] = 0
 
     return myPositions 
+    PrevPosition.append(myPositions)
 
 
 global prcSoFars
